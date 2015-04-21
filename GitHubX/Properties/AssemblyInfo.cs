37795171e7c4c848f8cbde4a0ea344712c87bdf3
Jongho Lee <ejjong7@gmail.com>
2015-04-21 17:09:48 +0900

    Add test project(xunit) On VS2013

--- GitHubX/Properties/AssemblyInfo.cs
+++ GitHubX/Properties/AssemblyInfo.cs
@@ -25,3 +25,5 @@ using System.Runtime.CompilerServices;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
+[assembly: InternalsVisibleToAttribute("GitHubX.Tests")]
+
---
+++ GitHubX/ViewModels/TestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubX.ViewModels
+{
+    public class TestViewModel
+    {
+        internal int Sum(int x , int y)
+        {
+            return x + y;
+        }
+    }
+}
