bc249cd634f6217d782fe6f3b5dee352f1731bdb
Jongho Lee <ejjong7@gmail.com>
2015-04-22 19:28:31 +0900

    add

---
+++ GitHubX/Views/DifferentView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ReactiveUI;
+using ReactiveUI.XamForms;
+using GitHubX.ViewModels;
+
+namespace GitHubX.Views
+{
+	public class DifferentViewBase : ReactiveContentPage<DifferentViewModel> { }
+
+	public partial class DifferentView : DifferentViewBase
+	{
+		public DifferentView ()
+		{
+			InitializeComponent ();
+
+			this.BindCommand(ViewModel, x => x.HostScreen.Router.NavigateBack, x => x.NavigateBack);
+		}
+	}
+}
+
+
